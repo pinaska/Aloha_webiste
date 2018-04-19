73f019ccbe17f510bb185223ee1ce4fc9d1ed77e
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-04-19 16:34:58 -0700

    Add jquery plugin | style and add jQuery to the form | minor styles changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
-  // $("h1").css("color", "red");
-  // var emailInput =
-  $('#subscribe_button').on('click', function (){
+//adding the pop-up for preventing the empty subscription
+$('form').on('submit', function(event){
     event.preventDefault();
-    var inputValue = $('input').val();
-    alert(inputValue);
-  // alert('Please submit a valid email address');
 
+    if ($("input[type='email']").val() == "") {
+      alert("Please enter a valid email addres");
+    } else {
+      alert("Thanks for subscribing!");
+    }
+  });
+
+//flickity
+$('.main-carousel').flickity({
+  // options
+  wrapAround:true,
+  contain: true,
+  freeScroll: true
 });
+
 });
