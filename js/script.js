22b34a99a06a4c12bd20bc872f3c446b3d9526d9
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-04-18 19:17:28 -0700

    add jquery| changes in gitignore

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('testin');
+$(document).ready(function(){
+  // $("h1").css("color", "red");
+  // var emailInput =
+  $('#subscribe_button').on('click', function (){
+    event.preventDefault();
+    var inputValue = $('input').val();
+    alert(inputValue);
+  // alert('Please submit a valid email address');
+
+});
+});
