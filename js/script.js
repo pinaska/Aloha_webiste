f98ce0f91eb9c0dbf49643cc14bcea086ae0447a
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-04-19 21:59:06 -0700

    SmoothNav | Cleaning up css

--- js/script.js
+++ js/script.js
@@ -7,10 +7,6 @@ $('.add-to-cart').click(function(){
     $('.cart-items').html(cartItems);
 });
 
-
-
-
-
 //adding the pop-up for preventing the empty subscription
 $('form').on('submit', function(event){
     event.preventDefault();
@@ -30,5 +26,40 @@ $('.main-carousel').flickity({
   freeScroll: true
 });
 
-//smooth scrolling
+//smooth scrolling src:https://css-tricks.com/snippets/jquery/smooth-scrolling/#article-header-id-1
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 });
